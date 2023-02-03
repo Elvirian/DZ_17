b803951c73adff7c4cc70d71a6939904d391062d
Elvirian <116085263+Elvirian@users.noreply.github.com>
2023-02-03 16:39:16 +0200

    DZ_17

---
+++ src/Tree.java
@@ -0,0 +1,39 @@
+public class Tree {
+    private Node tree;
+
+    public Node getTree() {
+        return tree;
+    }
+
+    public void addNode(int value) {
+        tree = addNode(tree, value);
+    }
+
+    private Node addNode(Node num, int value) {
+        if (num == null) {
+            return new Node(value);
+        }
+        if (value < num.getValue()) {
+            num.setLeft(addNode(num.getLeft(), value));
+        } else if (value > num.getValue()) {
+            num.setRight(addNode(num.getRight(), value));
+        } else {
+            return num;
+        }
+        return num;
+    }
+
+    public void printNode(){
+        Node node = this.getTree();
+        printNodeLeft(node);
+    }
+
+    private void printNodeLeft(Node num) {
+        if (num != null) {
+            printNodeLeft(num.getLeft());
+            System.out.print(num.getValue() + " ");
+            printNodeLeft(num.getRight());
+        }
+    }
+}
+
